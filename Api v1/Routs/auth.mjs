b2b35576c853289f14806f2d8a8226d5eb5b889e
M.Anas Khan <a4anas406@gmail.com>
2023-11-23 01:15:44 +0500

    login-signup api

--- Api v1/Routs/auth.mjs
+++ Api v1/Routs/auth.mjs
@@ -1,15 +1,130 @@
 import express from 'express';
-let router = express.Router()
+import { client } from '../../mongoDb.mjs';
+import { stringToHash, verifyHash, validateHash } from "bcrypt-inzi";
+import jwt from 'jsonwebtoken';
 
 
-router.post('/login', (req, res, next) => {
-    res.send('MongoDb Login ' + new Date);
-    console.log('MongoDb Login', new Date);
+const userCollection = client.db("crudDb").collection("users");
+let router = express.Router();
+
+// Regular expression for basic email validation
+// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+//-----------------------------------------------------------------------------------------------------
+//---------------------------------- LOGIN -------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------
+
+router.post('/login', async (req, res, next) => {
+
+    if (!req.body?.email || !req.body?.password) {
+        res.status(403).send("Required parameter missing")
+        return
+    }
+
+    req.body.email = req.body.email.toLowerCase();
+
+    try {
+        let result = await userCollection.findOne({ email: req.body.email });
+        console.log("result", result)
+
+        if (!result) { // user not found
+
+            res.status(401).send({
+                message: "email or password incorret"
+            });
+            return;
+
+        } else { // user found
+
+            const hashedPasswordMetch = await verifyHash(req.body.password, result.password)
+
+            if (hashedPasswordMetch) {
+
+                const token = jwt.sign({  // JWT token 
+                    isAdmin: false,
+                    firstName: result.firstName,
+                    lastName: result.lastName,
+                    email: req.body.email,
+                }, process.env.SECRET, {expiresIn: "720h"});
+
+                res.cookie("token", token, { // Cookies
+                    httpOnly: true,
+                    secure: true,
+                })
+
+                res.send({
+                    message: "Login successful",
+                    token: token
+                });
+                return;
+
+            } else {
+                res.status(401)({
+                    message: "email or password incorret"
+                })
+            }
+        }
+
+        // res.send(result);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Server error, please try leter")
+    }
+
+
+    // console.log('MongoDb Login', new Date);
 })
 
-router.post('/signup', (req, res, next) => {
-    res.send('MongoDb SignUp ' + new Date);
-    console.log('MongoDb SignUp ', new Date);
+//-----------------------------------------------------------------------------------------------------
+//---------------------------------- SIGNUP -------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------
+
+router.post('/signup', async (req, res, next) => {
+
+    if (!req.body?.firstName || !req.body?.lastName || !req.body?.email || !req.body?.password) {
+        res.status(403).send("Required parameter missing")
+        return
+    }
+
+    if (req.body.email) {
+
+    }
+
+    req.body.email = req.body.email.toLowerCase();
+
+    try {
+        let result = await userCollection.findOne({ email: req.body.email });
+        console.log("result", result)
+
+        if (!result) { // user not found
+
+            const hashedPassword = await stringToHash(req.body.password);
+
+            const insertResponse = await userCollection.insertOne({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: hashedPassword,
+                createdOn: new Date()
+            })
+
+            console.log("insertResponse", insertResponse);
+            res.send({ message: "SignUp successful" });
+
+        } else {
+            res.status(403).send({
+                message: "User already exist"
+            })
+        }
+
+        // res.send(result);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Server error, please try leter")
+    }
+
+    // console.log('MongoDb SignUp ', new Date);
 })
 
 export default router
--- server.mjs
+++ server.mjs
@@ -1,32 +1,57 @@
 // console.log("MongoDb express server>>>>>>>> ")
 
+// import apiv1Routs from './Api v1/main.mjs'
 import express from 'express';
-import cors from 'cors';
 import path from 'path';
-import apiv1Routs from './Api v1/main.mjs'
+import postRouter from './Api v1/Routs/post.mjs'
+import feedRouter from './Api v1/Routs/feed.mjs'
+import comentsRouter from './Api v1/Routs/comments.mjs'
+import autsRouter from './Api v1/Routs/auth.mjs'
+import "dotenv/config"
+import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken';
+import cors from 'cors';
 import mongodb from 'mongodb'
 
 const __dirname = path.resolve();
 
 
-
- 
-
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 // app.use(cors())
 
-app.use('/api/v1', apiv1Routs)
+app.use("/api/v1", autsRouter);
 
 app.use((req, res, next) => {
-  let token = "valid"
-  if (token === "valid") {
-    next()
-  } else {
-    res.send({ message: "invalid token" })
+  const token = req.cookies.token;
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    console.log("decoded", new Date(decoded));
+
+    req.body.decoded = {
+      isAdmin: decoded.isAdmin,
+      firstName: decoded.firstName,
+      lastName: decoded.lastName,
+      email: decoded.email,
+      // createdOn: decoded.createdOn,
+      // expires: decoded.expires
+    }
+    next();
+  } catch (err) {
+    res.status(401).send({ message: "invalid token" })
   }
+
+  console.log("cookieParser", req.cookies);
+
 })
 
+
+app.use('/api/v1', postRouter);
+app.use('/api/v1', comentsRouter);
+app.use('/api/v1', feedRouter);
+// app.use('/api/v1', apiv1Routs);
 
 
 
